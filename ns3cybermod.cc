b59852234b1d040e238613a410d3ac8e7ee832d1
moiznationzz <184729211+moiznationzz@users.noreply.github.com>
2025-12-20 20:02:09 +0500

    Refactor attack setup and logging functions

--- ns3cybermod.cc
+++ ns3cybermod.cc
@@ -8,16 +8,29 @@
 #include "ns3/socket.h"
 #include "ns3/simulator.h"
 #include <fstream>
+#include <cstdlib>
 
 namespace ns3 {
 
-/* ================= GLOBAL ================= */
-
+/* ================= GLOBAL VARIABLES ================= */
 uint64_t totalPacketsReceived[100] = {0};
 std::vector<Ptr<Packet>> capturedPackets;
+std::vector<Victim> victimRegistry;
+std::map<uint32_t, AttackStats> attackerStats;
+
+/* ================= VICTIM CONSTRUCTOR ================= */
+Victim::Victim(uint32_t id)
+    : victimId(id),
+      pcName("PC" + std::to_string(id + 1)),
+      cpuPower(2),
+      memoryMB(1024),
+      maxPacketRate(5000),
+      hasFirewall(false),
+      hasIntrusionDetection(false)
+{
+}
 
 /* ================= CALLBACKS ================= */
-
 void PacketReceivedCallback(uint32_t victimId,
                             Ptr<const Packet> packet,
                             const Address &from)
@@ -25,115 +38,99 @@ void PacketReceivedCallback(uint32_t victimId,
     totalPacketsReceived[victimId]++;
 }
 
-void TxLog(uint32_t attackerId, Ptr<const Packet> packet)
+void TxCounter(uint32_t attackerId, Ptr<const Packet> packet)
 {
-    NS_LOG_UNCOND("[TX] Attacker " << attackerId
-        << " sent packet at "
-        << Simulator::Now().GetSeconds() << "s");
-}
-
-void RxLog(uint32_t victimId,
-           Ptr<const Packet> packet,
-           const Address &from)
-{
-    NS_LOG_UNCOND("[RX] Victim " << victimId
-        << " received packet at "
-        << Simulator::Now().GetSeconds() << "s");
+    attackerStats[attackerId].packetsSent++;
 }
 
 /* ================= FLOOD ATTACK ================= */
-
 void SetupOnOffFlood(const SimParams& params,
                      const std::string& socketFactory)
 {
     uint32_t A = params.numAttackers;
     uint32_t V = params.numVictims;
+    uint32_t attackerLimit = (params.attackMode == "dos") ? 1 : A;
 
+    // Install PacketSink on victims
     for (uint32_t v = 0; v < V; v++)
     {
         PacketSinkHelper sink(
             socketFactory,
-            InetSocketAddress(
-                params.iface[0].GetAddress(A + v),
-                params.attackPort));
+            InetSocketAddress(params.iface[0].GetAddress(attackerLimit + v),
+                              params.attackPort));
 
-        ApplicationContainer sinkApp =
-            sink.Install(params.victims.Get(v));
-
+        ApplicationContainer sinkApp = sink.Install(params.victims.Get(v));
         sinkApp.Start(Seconds(0.0));
         sinkApp.Stop(Seconds(12.0));
 
-        Ptr<PacketSink> sinkPtr =
-            DynamicCast<PacketSink>(sinkApp.Get(0));
-
-        sinkPtr->TraceConnectWithoutContext(
-            "Rx", MakeBoundCallback(&RxLog, v));
-
+        Ptr<PacketSink> sinkPtr = DynamicCast<PacketSink>(sinkApp.Get(0));
         sinkPtr->TraceConnectWithoutContext(
             "Rx", MakeBoundCallback(&PacketReceivedCallback, v));
     }
 
-    for (uint32_t a = 0; a < A; a++)
+    // Install OnOffApplication on attackers
+    for (uint32_t a = 0; a < attackerLimit; a++)
     {
         for (uint32_t v = 0; v < V; v++)
         {
             OnOffHelper onoff(
                 socketFactory,
-                InetSocketAddress(
-                    params.iface[0].GetAddress(A + v),
-                    params.attackPort));
+                InetSocketAddress(params.iface[0].GetAddress(attackerLimit + v),
+                                  params.attackPort));
 
-            onoff.SetConstantRate(
-                DataRate(params.attackRate));
+            onoff.SetConstantRate(DataRate(params.attackRate));
+            onoff.SetAttribute("PacketSize", UintegerValue(512)); // slower port
 
-            onoff.SetAttribute(
-                "PacketSize", UintegerValue(1024));
-
-            ApplicationContainer app =
-                onoff.Install(params.attackers.Get(a));
-
+            ApplicationContainer app = onoff.Install(params.attackers.Get(a));
             app.Start(Seconds(1.0));
             app.Stop(Seconds(11.0));
 
-            Ptr<OnOffApplication> onoffApp =
-                DynamicCast<OnOffApplication>(app.Get(0));
+            Ptr<OnOffApplication> onoffApp = DynamicCast<OnOffApplication>(app.Get(0));
+            onoffApp->TraceConnectWithoutContext("Tx", MakeBoundCallback(&TxCounter, a));
 
-            onoffApp->TraceConnectWithoutContext(
-                "Tx", MakeBoundCallback(&TxLog, a));
+            // Store metadata for logging
+            attackerStats[a].attackType = params.attackType;
+            attackerStats[a].targetIp = params.iface[0].GetAddress(attackerLimit + v);
         }
     }
 }
 
 /* ================= IP SPOOFING ATTACK ================= */
-
 void SetupIpSpoofingAttack(const SimParams& params)
 {
     uint32_t A = params.numAttackers;
     uint32_t V = params.numVictims;
+    uint32_t attackerLimit = (params.attackMode == "dos") ? 1 : A;
 
-    for (uint32_t a = 0; a < A; a++)
+    for (uint32_t a = 0; a < attackerLimit; a++)
     {
         Ptr<Socket> rawSocket =
-            Socket::CreateSocket(
-                params.attackers.Get(a),
-                Ipv4RawSocketFactory::GetTypeId());
+            Socket::CreateSocket(params.attackers.Get(a),
+                                 Ipv4RawSocketFactory::GetTypeId());
 
-        rawSocket->SetAttribute(
-            "Protocol", UintegerValue(17)); // UDP
+        rawSocket->SetAttribute("Protocol", UintegerValue(17)); // UDP
+
+        // Randomly select victim to spoof
+        uint32_t spoofedVictimId = rand() % V;
+        Ipv4Address spoofedIp = params.iface[0].GetAddress(attackerLimit + spoofedVictimId);
+
+        NS_LOG_UNCOND("[IP SPOOFING] Attacker " << a
+            << " impersonating " << victimRegistry[spoofedVictimId].pcName
+            << " (IP: " << spoofedIp << ")");
+
+        attackerStats[a].attackType = params.attackType;
+        attackerStats[a].targetIp = spoofedIp;
 
         for (uint32_t v = 0; v < V; v++)
         {
             Simulator::Schedule(
                 Seconds(1.0 + a),
                 [=]() {
-                    Ptr<Packet> packet =
-                        Create<Packet>(512);
+                    Ptr<Packet> packet = Create<Packet>(512);
 
                     Ipv4Header ip;
-                    ip.SetSource(
-                        Ipv4Address("10.10.10.10")); // spoofed
-                    ip.SetDestination(
-                        params.iface[0].GetAddress(A + v));
+                    ip.SetSource(spoofedIp);
+                    ip.SetDestination(params.iface[0].GetAddress(attackerLimit + v));
                     ip.SetProtocol(17);
                     ip.SetPayloadSize(packet->GetSize());
 
@@ -144,19 +141,18 @@ void SetupIpSpoofingAttack(const SimParams& params)
                     packet->AddHeader(udp);
                     packet->AddHeader(ip);
 
-                    rawSocket->SendTo(
-                        packet,
-                        0,
-                        InetSocketAddress(
-                            params.iface[0].GetAddress(A + v),
-                            params.attackPort));
+                    rawSocket->SendTo(packet, 0,
+                                      InetSocketAddress(params.iface[0].GetAddress(attackerLimit + v),
+                                                        params.attackPort));
+
+                    // Count packets sent
+                    attackerStats[a].packetsSent++;
                 });
         }
     }
 }
 
 /* ================= REPLAY ATTACK ================= */
-
 void CapturePacket(Ptr<const Packet> packet)
 {
     capturedPackets.push_back(packet->Copy());
@@ -166,92 +162,88 @@ void SetupReplayAttack(const SimParams& params)
 {
     uint32_t A = params.numAttackers;
     uint32_t V = params.numVictims;
+    uint32_t attackerLimit = (params.attackMode == "dos") ? 1 : A;
 
     for (uint32_t v = 0; v < V; v++)
     {
-        PacketSinkHelper sink(
-            "ns3::UdpSocketFactory",
-            InetSocketAddress(
-                params.iface[0].GetAddress(A + v),
-                params.attackPort));
+        PacketSinkHelper sink("ns3::UdpSocketFactory",
+                              InetSocketAddress(params.iface[0].GetAddress(attackerLimit + v),
+                                                params.attackPort));
 
-        ApplicationContainer sinkApp =
-            sink.Install(params.victims.Get(v));
-
+        ApplicationContainer sinkApp = sink.Install(params.victims.Get(v));
         sinkApp.Start(Seconds(0.0));
         sinkApp.Stop(Seconds(12.0));
 
-        Ptr<PacketSink> sinkPtr =
-            DynamicCast<PacketSink>(sinkApp.Get(0));
-
-        sinkPtr->TraceConnectWithoutContext(
-            "Rx", MakeCallback(&CapturePacket));
+        Ptr<PacketSink> sinkPtr = DynamicCast<PacketSink>(sinkApp.Get(0));
+        sinkPtr->TraceConnectWithoutContext("Rx", MakeCallback(&CapturePacket));
     }
 
-    Simulator::Schedule(
-        Seconds(6.0),
+    Simulator::Schedule(Seconds(6.0),
         [&]() {
-            for (uint32_t a = 0; a < A; a++)
+            for (uint32_t a = 0; a < attackerLimit; a++)
             {
-                Ptr<Socket> sock =
-                    Socket::CreateSocket(
-                        params.attackers.Get(a),
-                        UdpSocketFactory::GetTypeId());
+                Ptr<Socket> sock = Socket::CreateSocket(params.attackers.Get(a),
+                                                        UdpSocketFactory::GetTypeId());
 
                 for (auto pkt : capturedPackets)
                 {
-                    sock->SendTo(
-                        pkt->Copy(),
-                        0,
-                        InetSocketAddress(
-                            params.iface[0].GetAddress(A),
-                            params.attackPort));
+                    sock->SendTo(pkt->Copy(), 0,
+                                 InetSocketAddress(params.iface[0].GetAddress(attackerLimit),
+                                                   params.attackPort));
+
+                    attackerStats[a].packetsSent++;
                 }
             }
         });
 }
 
 /* ================= ATTACK SELECTOR ================= */
-
 void SetupAttack(const SimParams& params)
 {
+    if (victimRegistry.empty())
+    {
+        for (uint32_t i = 0; i < params.numVictims; i++)
+            victimRegistry.emplace_back(i);
+    }
+
     if (params.attackType == "udp-flood")
         SetupOnOffFlood(params, "ns3::UdpSocketFactory");
-
     else if (params.attackType == "tcp-flood")
         SetupOnOffFlood(params, "ns3::TcpSocketFactory");
-
     else if (params.attackType == "icmp-flood")
         SetupOnOffFlood(params, "ns3::UdpSocketFactory");
-
     else if (params.attackType == "ip-spoofing")
         SetupIpSpoofingAttack(params);
-
     else if (params.attackType == "replay")
         SetupReplayAttack(params);
-
     else
         NS_ABORT_MSG("Unsupported attack type");
 }
 
-/* ================= SAVE LOGS ================= */
-
+/* ================= BEAUTIFUL LOGS ================= */
 void SaveLogs(const SimParams& params)
 {
-    std::ofstream file("ddos_log.txt");
+    NS_LOG_UNCOND("\n================ ATTACK SUMMARY ================\n");
 
-    file << "===== CSMA CYBER ATTACK LOG =====\n";
-    file << "Attack Type: " << params.attackType << "\n";
-    file << "Attackers : " << params.numAttackers << "\n";
-    file << "Victims   : " << params.numVictims << "\n";
-    file << "Rate      : " << params.attackRate << "\n\n";
+    NS_LOG_UNCOND("Attack Type  : " << params.attackType);
+    NS_LOG_UNCOND("Attack Mode  : " << params.attackMode);
+    NS_LOG_UNCOND("Attackers    : " << params.numAttackers);
+    NS_LOG_UNCOND("Victims      : " << params.numVictims);
+    NS_LOG_UNCOND("");
 
-    for (uint32_t i = 0; i < params.numVictims; i++)
-        file << "Victim " << i
-             << " packets received: "
-             << totalPacketsReceived[i] << "\n";
+    for (uint32_t a = 0; a < params.numAttackers; ++a)
+    {
+        NS_LOG_UNCOND("Attacker " << a
+            << " → Target IP " << attackerStats[a].targetIp
+            << " | Attack Type: " << attackerStats[a].attackType);
 
-    file.close();
+        NS_LOG_UNCOND("Packets Sent     : " << attackerStats[a].packetsSent);
+        NS_LOG_UNCOND("Packets Received : " 
+            << totalPacketsReceived[a % params.numVictims]);
+        NS_LOG_UNCOND("-------------------------------------------");
+    }
+
+    NS_LOG_UNCOND("===============================================\n");
 }
 
 } // namespace ns3
